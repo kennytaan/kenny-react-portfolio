cd3f45ca4d6b007d168ace0d0fe5f5e8472c2d3a
Kenny <79708611+kennytaan@users.noreply.github.com>
2023-02-28 19:20:45 +0800

    update website title

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactGA from 'react-ga';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactGA.initialize('UA-258302878-1');
 ReactGA.event({'category':'Test', 'action':'submit','label':'label'});
 
 function App() {
+  
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>{"<Kenny/>"}</title>
+      </Helmet>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
 }
 
